e3e6177c017ada29784108b6e30754e6fb875c38
Amir Shalem <amir@boom.org.il>
2005-07-27 12:19:01 +0000

    remove (also) all object files when packaging.

--- package.sh
+++ package.sh
@@ -5,5 +5,5 @@ DIR="`tla pristines | cut -f 2 -d /`"
 rm -rf "$DIR"
 mkdir -p "$DIR"
 cp -pPR *.cxx *.h *.fl generator.exe Dependencies Makefile Fl_Gel_Tabs "$DIR"
-find "$DIR" -name .arch-ids -exec rm -rf '{}' \; 2>/dev/null
+find "$DIR" \( -name .arch-ids -or -name '*.o' \) -exec rm -rf '{}' \; 2>/dev/null
 tar -czf "$DIR.tar.gz" "$DIR"

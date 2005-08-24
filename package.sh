6f4bc2f9b0e3dd2f368b24f5ef43e04ba261b5f8
Amir Shalem <amir@boom.org.il>
2005-08-24 15:56:40 +0000

    rename linux-generator to linux-i386-generator

--- package.sh
+++ package.sh
@@ -2,7 +2,7 @@
 
 DIR="`tla pristines | cut -f 2 -d /`"
 
-for i in generator.exe linux-generator macosx-generator generatorUI.cxx generatorUI.h; do
+for i in generator.exe linux-i386-generator macosx-generator generatorUI.cxx generatorUI.h; do
   if [ ! -f "$i" ]; then
     echo "$i is missing"
     exit 1
@@ -11,6 +11,6 @@ done
 
 rm -rf "$DIR"
 mkdir -p "$DIR"
-cp -pPR *.cxx *.h *.fl linux-generator macosx-generator generator.exe Dependencies Makefile Fl_Gel_Tabs "$DIR"
+cp -pPR *.cxx *.h *.fl linux-i386-generator macosx-generator generator.exe Dependencies Makefile Fl_Gel_Tabs "$DIR"
 find "$DIR" \( -name .arch-ids -or -name '*.o' \) -exec rm -rf '{}' \; 2>/dev/null
 tar -czf "$DIR.tar.gz" "$DIR"

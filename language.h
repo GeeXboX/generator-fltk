7db0d326023bee5f05d973bdc53c12b1021f6704
Amir Shalem <amir@boom.org.il>
2006-01-20 14:07:54 +0000

    update copyright years to 2006

--- language.h
+++ language.h
@@ -1,6 +1,6 @@
 /*
  *  Language support for GeeXboX FLTK Generator
- *  Copyright (C) 2005  Amir Shalem
+ *  Copyright (C) 2005-2006  Amir Shalem
  *
  *   This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by

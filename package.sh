c0b407c221a40c435daad6493b0b21393e6cccf4
Amir Shalem <amir@boom.org.il>
2006-04-29 17:50:37 +0000

    macosx generator no longer requires dmg for resources

--- package.sh
+++ package.sh
@@ -2,7 +2,7 @@
 
 DIR="`tla pristines | tail -1 | cut -f 2 -d /`"
 
-for i in generator.exe linux-i386-generator macosx-generator.dmg generatorUI.cxx generatorUI.h; do
+for i in generator.exe linux-i386-generator macosx-generator generatorUI.cxx generatorUI.h; do
   if [ ! -f "$i" ]; then
     echo "$i is missing"
     exit 1
@@ -11,6 +11,6 @@ done
 
 rm -rf "$DIR"
 mkdir -p "$DIR"
-cp -pPR *.c *.cxx *.h *.fl linux-i386-generator macosx-generator.dmg generator.exe Dependencies Makefile Fl_Gel_Tabs FLU libmd libbz2 "$DIR"
+cp -pPR *.c *.cxx *.h *.fl linux-i386-generator macosx-generator generator.exe Dependencies Makefile Fl_Gel_Tabs FLU libmd libbz2 "$DIR"
 find "$DIR" \( -name .arch-ids -or -name '*.o' \) -exec rm -rf '{}' \; 2>/dev/null
 tar -czf "$DIR.tar.gz" "$DIR"

5b634390ba32c9b593cd0d21c667618f4929c067
Mathieu Schroeter <mathieu.schroeter@mycable.ch>
2008-06-16 13:33:22 +0200

    updated copyright

--- theme.cxx
+++ theme.cxx
@@ -1,6 +1,7 @@
 /*
  *  Theme support for GeeXboX FLTK Generator
- *  Copyright (C) 2005-2006  Amir Shalem
+ *  Copyright (C) 2005-2008  Amir Shalem
+ *                           Mathieu Schroeter
  *
  *   This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
